0dfe861406ddcf35baa881b7c3d9ff1ea67ec122
Andrew Shini <superandrew213@gmail.com>
2017-06-01 13:25:50 +1000

    Remove dead code

--- src/components/Echarts/index.js
+++ src/components/Echarts/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { WebView, View, StyleSheet } from 'react-native';
 import renderChart from './renderChart';
-import echarts from './echarts.min';
 
 export default class App extends Component {
   componentWillReceiveProps(nextProps) {
