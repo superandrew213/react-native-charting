0499a6bb947c68aea04008d4e95526f485dae809
Andrew Shini <superandrew213@gmail.com>
2017-06-10 20:11:12 +1000

    Improve code

--- src/components/Echarts/renderChart.js
+++ src/components/Echarts/renderChart.js
@@ -1,17 +1,17 @@
-import toString from '../../util/toString';
+import toString from '../../utils/toString';
 
 export default props => {
   const height = `${props.height || 400}px`;
   const width = props.width ? `${props.width}px` : 'auto';
   return `
-    document.getElementById('main').style.height = "${height}";
-    document.getElementById('main').style.width = "${width}";
+    document.getElementById('main').style.height = '${height}';
+    document.getElementById('main').style.width = '${width}';
     var myChart = echarts.init(document.getElementById('main'));
     myChart.setOption(${toString(props.option)});
     myChart.on('click', function(params) {
       var seen = [];
       var paramsString = JSON.stringify(params, function(key, val) {
-        if (val != null && typeof val == "object") {
+        if (typeof val === 'object') {
           if (seen.indexOf(val) >= 0) {
             return;
           }
