df9277a4b5ee21ce62dee45effb5f391fb7727a4
Andrew Shini <superandrew213@gmail.com>
2019-08-19 14:43:29 +1000

    Use window.ReactNativeWebView.postMessage

--- src/components/Echarts/renderChart.js
+++ src/components/Echarts/renderChart.js
@@ -1,25 +1,27 @@
-import toString from '../../utils/toString';
+import toString from '../../utils/toString'
 
 export default props => {
-  const height = `${props.height || 400}px`;
-  const width = props.width ? `${props.width}px` : 'auto';
+  const height = `${props.height || 400}px`
+  const width = props.width ? `${props.width}px` : 'auto'
   return `
-    document.getElementById('main').style.height = '${height}';
-    document.getElementById('main').style.width = '${width}';
-    var myChart = echarts.init(document.getElementById('main'));
-    myChart.setOption(${toString(props.option)});
-    myChart.on('click', function(params) {
-      var seen = [];
-      var paramsString = JSON.stringify(params, function(key, val) {
-        if (typeof val === 'object') {
-          if (seen.indexOf(val) >= 0) {
-            return;
-          }
-          seen.push(val);
+(function() {
+  document.getElementById('main').style.height = '${height}';
+  document.getElementById('main').style.width = '${width}';
+  var myChart = echarts.init(document.getElementById('main'));
+  myChart.setOption(${toString(props.option)});
+  myChart.on('click', function(params) {
+    var seen = [];
+    var paramsString = JSON.stringify(params, function(key, val) {
+      if (typeof val === 'object') {
+        if (seen.indexOf(val) >= 0) {
+          return;
         }
-        return val;
-      });
-      window.postMessage(paramsString);
+        seen.push(val);
+      }
+      return val;
     });
-  `;
-};
+    window.ReactNativeWebView.postMessage(paramsString);
+  });
+})();
+  `
+}
