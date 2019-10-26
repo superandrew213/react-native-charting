a9f40fa45a2454b2a9c7935d626cc26a75c14572
Andrew Shini <superandrew213@gmail.com>
2019-10-26 17:23:14 +1000

    Fix Android - can use uri now

--- src/components/Echarts/index.js
+++ src/components/Echarts/index.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
-import { View, StyleSheet, Platform } from 'react-native';
+import React, { Component } from 'react'
+import { View, StyleSheet, Platform } from 'react-native'
 import WebView from 'react-native-webview'
-import renderChart from './renderChart';
+import renderChart from './renderChart'
 
 const SOURCE = Platform.select({
   ios: require('./echart.html'),
-  android: __DEV__
-    ? require('./echart.html')
-    : { uri: 'file:///android_asset/echart.html' },
-});
+  android: { uri: 'file:///android_asset/echart.html' }
+})
 
 export default class extends Component {
   componentWillReceiveProps(nextProps) {
     if (nextProps.option !== this.props.option) {
-      this.chart.reload();
+      this.chart.reload()
     }
   }
 
@@ -21,19 +19,20 @@ export default class extends Component {
     return (
       <View style={{ flex: 1, height: this.props.height || 400 }}>
         <WebView
-          ref={ref => this.chart = ref}
+          ref={ref => (this.chart = ref)}
           style={{ height: this.props.height || 400 }}
           source={SOURCE}
           injectedJavaScript={renderChart(this.props)}
-          onMessage={event => this.props.onPress
-            ? this.props.onPress(JSON.parse(event.nativeEvent.data))
-            : {}
+          onMessage={event =>
+            this.props.onPress
+              ? this.props.onPress(JSON.parse(event.nativeEvent.data))
+              : {}
           }
           scrollEnabled={false}
           originWhitelist={['*']}
           scalesPageToFit={Platform.select({ android: false })}
         />
       </View>
-    );
+    )
   }
 }
