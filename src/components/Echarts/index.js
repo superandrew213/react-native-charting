5bcf76629f596b3cc80f82ea9039fe1018827f97
Andrew Shini <superandrew213@gmail.com>
2020-06-10 12:24:30 +1000

    Stop parent scroll when scrolling in chart for Android
    iOS does this by default.

--- src/components/Echarts/index.js
+++ src/components/Echarts/index.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
-import { View, StyleSheet, Platform } from 'react-native'
+import { View, StyleSheet, Platform, PanResponder } from 'react-native'
 import WebView from 'react-native-webview'
 import renderChart from './renderChart'
 
 const SOURCE = Platform.select({
   ios: require('./echart.html'),
   android: { uri: 'file:///android_asset/echart.html' },
+})
+
+const panResponder = PanResponder.create({
+  onPanResponderTerminationRequest: () => false,
+  onStartShouldSetPanResponderCapture: () => true,
 })
 
 export default class extends Component {
@@ -22,11 +27,11 @@ export default class extends Component {
     return (
       <View style={{ flex: 1, height: this.props.height || 400 }}>
         <WebView
-          ref={(ref) => (this.chart = ref)}
+          ref={ref => (this.chart = ref)}
           style={{ height: this.props.height || 400 }}
           source={SOURCE}
           injectedJavaScript={renderChart(this.props)}
-          onMessage={(event) => {
+          onMessage={event => {
             const { type } = JSON.parse(event.nativeEvent.data)
             if (type === 'click' && this.props.onPress) {
               this.props.onPress(JSON.parse(event.nativeEvent.data))
@@ -40,6 +45,9 @@ export default class extends Component {
           scrollEnabled={false}
           originWhitelist={['*']}
           scalesPageToFit={Platform.select({ android: false })}
+          // To stop parent receiving touch events and scroll etc
+          // Only required for Android (iOS already does this) but let's keep it for iOS too
+          {...panResponder.panHandlers}
         />
       </View>
     )
