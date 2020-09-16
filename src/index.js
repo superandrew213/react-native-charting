49f68643fd50628996f530b187efe40ca790b2f8
Andrew Shini <superandrew213@gmail.com>
2020-09-16 16:40:12 +1000

    Allow to get chart ref

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react'
 import { Container, Echarts } from './components'
 
 export default class App extends Component {
+  getChartRef() {
+    return this.echarts && this.echarts.chart ? this.echarts.chart : undefined
+  }
+
   render() {
     return (
       <Container width={this.props.width}>
-        <Echarts {...this.props} />
+        <Echarts ref={ref => (this.echarts = ref)} {...this.props} />
       </Container>
     )
   }
